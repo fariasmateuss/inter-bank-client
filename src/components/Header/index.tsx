dc387c744e2592e7126149d16c13411ab7d5a857
Mateus V. Farias <mateus_vinicius15@outlook.com>
2021-12-31 01:31:06 -0400

    feat: fetches user name and get initials

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,22 +1,34 @@
 import { Link } from 'react-router-dom';
 
 import logo from '@assets/Inter-orange.png';
+
 import { Initials } from '@components/Initials';
+import { useAuth } from '@hooks/useAuth';
 
 import { CustomHeader } from './styles';
 
 export function Header() {
+  const { user } = useAuth();
+  const initials =
+    user.firstName.substring(0, 1) + user.lastName.substring(0, 1);
+
   return (
     <CustomHeader>
-      <img src={logo} alt="Bank Inter" />
+      <img src={logo} alt="Bank Inter Logo" />
 
       <div>
-        <Initials initials="JD" />
+        <Initials initials={initials} />
 
         <div>
           <p>
-            Hi, <span>John Doe</span>
+            Hi,{` `}
+            <span>
+              {user.firstName} {user.lastName}
+            </span>
           </p>
+          <strong>
+            {user.accountNumber}-{user.accountDigit}
+          </strong>
 
           <Link to="/">Logoff</Link>
         </div>
